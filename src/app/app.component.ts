a3812fb37b5518e1bf8bc6ee0421ee28f769fd59
skori <skorik.roma@gmail.com>
2022-12-13 21:10:07 +0900

    angular upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,89 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cars-app';
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required],
+  })
+
+  carsData = [
+    {
+      image: "1.png",
+      name: "Lamborghini Huracan Spyder",
+      transmission: "автомат",
+      engine: 5.2, 
+      year: 2020
+    },
+    {
+      image: "2.png",
+      name: "Chevrolet Corvette",
+      transmission: "автомат",
+      engine: 6.2, 
+      year: 2021
+    },
+    {
+      image: "3.png",
+      name: "Ferrari California",
+      transmission: "автомат",
+      engine: 3.9, 
+      year: 1997
+    },
+    {
+      image: "4.png",
+      name: "Lamborghini Urus",
+      transmission: "автомат",
+      engine: 4.0, 
+      year: 2018
+    },
+    {
+      image: "5.png",
+      name: "Audi R8",
+      transmission: "автомат",
+      engine: 5.2, 
+      year: 2016
+    },
+    {
+      image: "6.png",
+      name: "Chevrolet Camaro",
+      transmission: "автомат",
+      engine: 2.0, 
+      year: 1990
+    },
+    {
+      image: "7.png",
+      name: "Maserati Quattroporte",
+      transmission: "автомат",
+      engine: 2.0, 
+      year: 1999
+    },
+    {
+      image: "8.png",
+      name: "Dodge Challenger",
+      transmission: "автомат",
+      engine: 2.0, 
+      year: 1980
+    },
+    {
+      image: "9.png",
+      name: "Nissan GT-R",
+      transmission: "автомат",
+      engine: 2.0, 
+      year: 2000
+    }
+  ]
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  goScroll(targer: HTMLElement) {
+    targer.scrollIntoView({behavior: "smooth"})
+  }
+
+  onSubmit() {
+    if (this.priceForm.valid) {
+      alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!")
+      this.priceForm.reset()
+    }
+  }
 }
